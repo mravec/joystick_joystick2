4b78db984bc1992550e430de69ae64dd0ab545ff
mravec <48887485+mravec@users.noreply.github.com>
2026-03-28 14:56:15 +0100

    Počiatočné súbory pre projekt MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+let y = 0
+let x = 0
+basic.showLeds(`
+    . # # . .
+    # . . . .
+    . # # . .
+    . . . # .
+    . # # . .
+    `)
+radio.setGroup(1)
+basic.forever(function () {
+    x = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.X), 1023, 0, -100, 100)
+    y = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.Y), 1023, 0, -100, 100)
+    radio.sendValue("x", x)
+    radio.sendValue("y", y)
+})
