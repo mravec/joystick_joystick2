d8da122abf574faef906f84a36263b5359f64231
mravec <48887485+mravec@users.noreply.github.com>
2026-03-28 16:05:09 +0100

    init joystick

--- main.ts
+++ main.ts
@@ -1,33 +1,41 @@
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
-    radio.sendValue("t", 5)
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.up, function () {
+    radio.sendValue("t", 3)
+    basic.showNumber(3)
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.up, function () {
+    radio.sendValue("t", 4)
+    basic.showNumber(4)
 })
 input.onButtonPressed(Button.A, function () {
     radio.sendValue("t", 1)
+    basic.showNumber(1)
 })
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.up, function () {
     radio.sendValue("t", 6)
+    basic.showNumber(6)
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendValue("t", 2)
+    basic.showNumber(2)
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.up, function () {
+    radio.sendValue("t", 5)
+    basic.showNumber(5)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     radio.sendValue("t", 0)
-})
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
-    radio.sendValue("t", 4)
-})
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
-    radio.sendValue("t", 3)
+    basic.showNumber(0)
 })
 let y = 0
 let x = 0
 basic.showLeds(`
-    # # # # #
-    . . # . .
-    . . # . .
-    . . # . .
-    . . # . .
+    . . . . #
+    . . . . #
+    . . . . #
+    . # . . #
+    . . # # .
     `)
+joystickbit.initJoystickBit()
 radio.setGroup(1)
 basic.forever(function () {
     x = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.X), 1023, 0, -100, 100)
