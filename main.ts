bb23806e4e392fda7bd7c000dfc895fd4da29360
mravec <48887485+mravec@users.noreply.github.com>
2026-03-28 15:14:14 +0100

    nove tlacitka

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
+    radio.sendValue("t", 5)
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendValue("t", 1)
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
+    radio.sendValue("t", 6)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendValue("t", 2)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendValue("t", 0)
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
+    radio.sendValue("t", 4)
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
+    radio.sendValue("t", 3)
+})
 let y = 0
 let x = 0
 basic.showLeds(`
